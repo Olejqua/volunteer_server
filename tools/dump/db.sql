ceaf9c75e99f94b436003a6dc226c4b97f4a3ecb
d.s.pustovit <ditrix1993@gmail.com>
2019-09-28 16:50:31 +0500

    sql

--- tools/dump/db.sql
+++ tools/dump/db.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 10.10 (Ubuntu 10.10-1.pgdg18.04+1)
+-- Dumped from database version 11.5 (Ubuntu 11.5-1.pgdg18.04+1)
 -- Dumped by pg_dump version 11.5 (Ubuntu 11.5-1.pgdg18.04+1)
 
 SET statement_timeout = 0;
@@ -139,6 +139,41 @@ ALTER SEQUENCE public.roles_id_seq OWNED BY public.roles.id;
 
 
 --
+-- Name: users_data; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users_data (
+    id integer NOT NULL,
+    reputation integer,
+    coins integer
+);
+
+
+ALTER TABLE public.users_data OWNER TO postgres;
+
+--
+-- Name: users_data_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_data_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_data_id_seq OWNER TO postgres;
+
+--
+-- Name: users_data_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_data_id_seq OWNED BY public.users_data.id;
+
+
+--
 -- Name: event id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -157,6 +192,13 @@ ALTER TABLE ONLY public.events ALTER COLUMN id SET DEFAULT nextval('public.event
 --
 
 ALTER TABLE ONLY public.roles ALTER COLUMN id SET DEFAULT nextval('public.roles_id_seq'::regclass);
+
+
+--
+-- Name: users_data id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users_data ALTER COLUMN id SET DEFAULT nextval('public.users_data_id_seq'::regclass);
 
 
 --
@@ -182,9 +224,9 @@ oleg	1	2019-09-27 22:00:00	2019-09-28 09:00:00	10	Ночной режим раб
 --
 
 COPY public.events (entity, type, action, params, active, priority, id, date_start, date_end, img, place) FROM stdin;
-Россия страна возможностей	конкурс	всероссийский конкурс для ИТ-специалистов, дизайнеров и управленцев в сфере цифровой экономики	О проекте\nЦифровой прорыв — новый конкурс платформы «Россия – страна возможностей».\n\nЗарегистрируйся на сайте, пройди онлайн-тестирование и участвуй в одном из 40 хакатонов в регионах России.\n\nЛучшие 	t	1	1	2019-11-20 10:00:00	2019-11-23 18:00:00	\N	Тюмень
-Фонд продовольствия Русь и X5 Retail Group	Помощь пожилым	Общегородской продовольственный марафон "Корзина доброты"	30 октября с 14:00 до 18:00 в Москве состоится общегородской продовольственный марафон «Корзина доброты» в  всех магазинах «Пятерочка» в пределах МКАД.\n\nВ ходе марафона волонтеры Фонда соберут #ПРОДУК	t	1	2	2019-10-30 14:00:00	2019-10-30 18:00:00	\N	Санкт-Петербург
-БФ "Волонтеры в помощь детям-сиротам"	Помощь детям	Приглашаем на акцию по сбору помощи в гипермаркете "Глобус"	Дорогие друзья!\n28 сентября с 11.00 до 21.00 состоится акция по сбору помощи для подопечных нашего фонда, которая пройдет в гипермаркете ГЛОБУС на Новорижском шоссе\nЧто делают волонтеры?\nНа акции воло	t	1	3	2019-12-13 09:00:00	2019-12-14 18:00:00	\N	Республика Татарстан
+Россия страна возможностей	конкурс	всероссийский конкурс для ИТ-специалистов, дизайнеров и управленцев в сфере цифровой экономики	О проекте\nЦифровой прорыв — новый конкурс платформы «Россия – страна возможностей».\n\nЗарегистрируйся на сайте, пройди онлайн-тестирование и участвуй в одном из 40 хакатонов в регионах России.\n\nЛучшие 	t	1	1	2019-11-20 10:00:00	2019-11-23 18:00:00	\N	Казань Экспо
+Фонд продовольствия Русь и X5 Retail Group	Помощь пожилым	Общегородской продовольственный марафон "Корзина доброты"	30 октября с 14:00 до 18:00 в Москве состоится общегородской продовольственный марафон «Корзина доброты» в  всех магазинах «Пятерочка» в пределах МКАД.\n\nВ ходе марафона волонтеры Фонда соберут #ПРОДУК	t	1	2	2019-10-30 14:00:00	2019-10-30 18:00:00	pjaterochka.jpg	Казань Экспо
+БФ "Волонтеры в помощь детям-сиротам"	Помощь детям	Приглашаем на акцию по сбору помощи в гипермаркете "Глобус"	Дорогие друзья!\n28 сентября с 11.00 до 21.00 состоится акция по сбору помощи для подопечных нашего фонда, которая пройдет в гипермаркете ГЛОБУС на Новорижском шоссе\nЧто делают волонтеры?\nНа акции воло	t	1	3	2019-12-13 09:00:00	2019-12-14 18:00:00	2f5bda7378ed531edfafd0794f3403c5.jpg	Казань Экспо
 \.
 
 
@@ -196,6 +238,14 @@ COPY public.roles (name, id) FROM stdin;
 admin	1
 organizer	2
 volunteer	3
+\.
+
+
+--
+-- Data for Name: users_data; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users_data (id, reputation, coins) FROM stdin;
 \.
 
 
@@ -221,6 +271,13 @@ SELECT pg_catalog.setval('public.roles_id_seq', 3, true);
 
 
 --
+-- Name: users_data_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_data_id_seq', 1, false);
+
+
+--
 -- Name: event event_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -242,6 +299,14 @@ ALTER TABLE ONLY public.events
 
 ALTER TABLE ONLY public.roles
     ADD CONSTRAINT roles_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users_data users_data_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users_data
+    ADD CONSTRAINT users_data_pkey PRIMARY KEY (id);
 
 
 --
