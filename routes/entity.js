d359f3b40a4bd1c6df561aeca09bfcb6830be188
d.s.pustovit <ditrix1993@gmail.com>
2019-09-28 22:48:14 +0500

    id entity

--- routes/entity.js
+++ routes/entity.js
@@ -6,7 +6,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res) => {
   db('events')
-    .select('entity', 'action', 'date_start', 'date_end', 'place', 'img')
+    .select('id', 'entity', 'action', 'date_start', 'date_end', 'place', 'img')
     .then((res2) => {
       res.send({ entitys: res2 });
     });
@@ -17,7 +17,7 @@ router.get('/:id', function (req, res, next) {
   .select('entity', 'action', 'date_start', 'date_end', 'place', 'img')
   .where('id', req.params.id)
   .then((res2) => {
-    res.send({ entity: res2 });
+    res.send({ entity: res2[0] });
   });
 });
 module.exports = router;
